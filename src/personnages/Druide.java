2efe98d6cad8f0057f0bb7b6f4f89461e28d022c
OlivierFournie <73936438+OlivierFournie@users.noreply.github.com>
2022-10-05 15:13:32 +0200

    modifications class

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -5,21 +5,19 @@ public class Druide {
 	private int effetPotionMin;
 	private int effetPotionMax;
 	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
-	this.nom = nom;
-	this.effetPotionMin = effetPotionMin;
-	this.effetPotionMax = effetPotionMax;
-	parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller
-	d'une force " + effetPotionMin + " à "
-	+ effetPotionMax + ".");
+		this.nom = nom;
+		this.effetPotionMin = effetPotionMin;
+		this.effetPotionMax = effetPotionMax;
+		parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller d'une force " + effetPotionMin + " à " + effetPotionMax + ".");
 	}
 	public String getNom() {
-	return nom;
+		return nom;
 	}
 	public void parler(String texte) {
-	System.out.println(prendreParole() + "« " + texte + "»");
+		System.out.println(prendreParole() + "« " + texte + "»");
 	}
 	private String prendreParole() {
-	return "Le druide " + nom + " : ";
+		return "Le druide " + nom + " : ";
 	}
 
 }
